87fab52266eb4a4ed5bc51b1cc4004efaaa203cb
Adrian Bao <adrianbao@adrians-macbook-pro.local>
2018-07-18 10:30:22 -0700

    Insert place holder based on user touch location.

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,4 +41,63 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            // Add placemarker
+            if let hitResult = hitTestResults.first {
+                addDot(at: hitResult)
+            } else {
+                // no valid location exists
+            }
+        } else {
+            // not valid touch exists
+        }
+    }
+    
+    func addDot(at hitResult: ARHitTestResult) {
+        
+        let dotGeomety = SCNSphere(radius: 0.005)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        dotGeomety.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeomety)
+        
+        // Assign position using hitResult
+        dotNode.position = SCNVector3(
+            x: hitResult.worldTransform.columns.3.x,
+            y: hitResult.worldTransform.columns.3.y,
+            z: hitResult.worldTransform.columns.3.z
+        )
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
